cf9ef2058eba51e7c378f845b07ce22c7246592f
libinjareo <libinjareo@163.com>
2019-11-21 15:00:52 +0800

    project init

---
+++ src/main/java/com/binsoft/film/BinFilmApplication.java
@@ -0,0 +1,13 @@
+package com.binsoft.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binsoft/film/BinFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binsoft.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
